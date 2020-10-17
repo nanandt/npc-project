2d85af4b8cdea5a62ee4d08f6e6894eeec337272
nanandt <rizkyfatih007@gmail.com>
2020-10-17 22:33:32 +0700

    mengembalikan file yg kehapus

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'HomeController@index')
+    ->name('home');
+
+Route::get('/jadwal-latihan', 'JadwalController@index')
+    ->name('jadwal-latihan');
+
+Route::get('/about', 'AboutController@index')
+    ->name('about');
+
+Route::get('/contact', 'ContactController@index')
+    ->name('contact');
+Route::post('/contact', 'ContactController@create')
+        ->name('contact-create');
+
+Route::get('/berita', 'BeritaController@index')
+    ->name('berita');
+
+Route::prefix('admin')
+        ->middleware('auth')
+        ->namespace('Admin')
+        ->group(function(){
+          Route::get('/', 'DashboardController@index')
+        ->name('dashboard');
+
+          Route::resource('posts', PostController::class);
+          Route::resource('messages', MessageController::class);
+          Route::resource('cabang-olahraga', CabangOlahragaController::class);
+          Route::resource('players', PlayerController::class);
+          Route::resource('coachs', CoachController::class);
+});
+
+Auth::routes();
