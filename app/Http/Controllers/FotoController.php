9508ead035efbd30290ce125bfc77cc0eb322678
nanandt <rizkyfatih007@gmail.com>
2020-12-16 13:52:46 +0700

    ngerapihin

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -11,15 +11,10 @@ class FotoController extends Controller
     public function index()
     {
         $cabors = CabangOlahraga::with(['pemains', 'pelatihs'])->get();
-<<<<<<< HEAD
-        $cabors1 = CabangOlahraga::with(['pemains', 'pelatihs'])->take(3)->get();
-        $items = CabangOlahraga::with(['pemains', 'pelatihs'])->skip(3)->take(3)->get();
-=======
-        
+
         $items = Photo::latest()->paginate(12);
-        
-        
->>>>>>> 382111bc0cc5702afde1c67f6cdd7b9cfff76aab
+
+
         return view('pages.foto', [
             'cabors' => $cabors,
             'items' => $items
